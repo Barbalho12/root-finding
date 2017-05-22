1e8bb5e07881a4df92e3596587c81642dff753c9
Felipe Barbalho <felipebarbalho.95@gmail.com>
2017-05-22 02:36:39 +0000

    Criação de estrutura

---
+++ compile.sh
@@ -0,0 +1,5 @@
+# echo  "Compilando xxx... "
+# g++ -std=c++11  src/xxx.cpp -o bin/xxx.exe
+
+
+
---
+++ run.sh
@@ -0,0 +1,20 @@
+# MA="matrizA.txt"
+# VB="vetorB.txt"
+# N=3
+# N=$2
+
+
+# if [ "$1" = 'l' ]; then
+#   ./bin/fatoracao_lu.exe fatoracao_lu/$MA fatoracao_lu/$VB $N $N
+# elif [ "$1" = 'c'  ]; then
+#   ./bin/cholesky.exe cholesky/$MA cholesky/$VB $N $N
+# elif [ "$1" = 'j'  ]; then
+#   ./bin/jacobi.exe jacobi/$MA jacobi/$VB $N $N
+# elif [ "$1" = 'g'  ]; then
+#   ./bin/gauss_seidel.exe gauss_seidel/$MA gauss_seidel/$VB $N $N
+# else
+#     echo " l = Fatoração LU"
+#     echo " c = Cholesky"
+#     echo " j = Jacobi"
+#     echo " g = Gauss Seidel"
+# fi
